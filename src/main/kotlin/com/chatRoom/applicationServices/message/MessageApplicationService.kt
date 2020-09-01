fe82d1a735ca62c896cd511437a0380dc282b52a
shiiyan <yan@softbank126051183211.bbtec.net>
2020-09-01 22:49:41 +0900

    create message application service

---
+++ src/main/kotlin/com/chatRoom/applicationServices/message/MessageApplicationService.kt
@@ -0,0 +1,21 @@
+package com.chatRoom.applicationServices.message
+
+import com.chatRoom.domainModels.message.MessageDomainService
+import com.chatRoom.repositories.message.IMessageRepository
+import com.chatRoom.repositories.room.IRoomRepository
+
+class MessageApplicationService(
+    private val messageRepository: IMessageRepository,
+    private val roomRepository: IRoomRepository
+) {
+    fun sendMessage(text: String, imagePaths: List<String>, roomId: String): String {
+
+        return MessageDomainService(messageRepository, roomRepository)
+            .sendMessage(text, imagePaths, roomId)
+    }
+
+    fun editMessage(messageId: String, newText: String, newImagePaths: List<String>) {
+        MessageDomainService(messageRepository, roomRepository)
+            .editMessage(messageId, newText, newImagePaths)
+    }
+}
