0f14e4c555360414f73574e798b611ca15eed2d1
shiiyan <yan@softbank126074195018.bbtec.net>
2020-09-14 18:29:46 +0900

    fix import

--- src/main/kotlin/com/chatRoom/applicationServices/thread/ThreadApplicationService.kt
+++ src/main/kotlin/com/chatRoom/applicationServices/thread/ThreadApplicationService.kt
@@ -3,14 +3,16 @@ package com.chatRoom.applicationServices.thread
 import com.chatRoom.domainModels.message.MessageId
 import com.chatRoom.domainModels.thread.ThreadDomainService
 import com.chatRoom.repositories.thread.IThreadRepository
+import com.chatRoom.repositories.thread.message.IMessageRepository
 import kotlin.concurrent.thread
 
 class ThreadApplicationService(
-    private val threadRepository: IThreadRepository
+    private val threadRepository: IThreadRepository,
+    private val messageRepository: IMessageRepository
 
 ) {
     fun createThread(messageId: String): String =
-        ThreadDomainService(threadRepository).createThread(messageId)
+        ThreadDomainService(threadRepository, messageRepository).createThread(messageId)
 
     fun getThreadId(messageId: String): String? = threadRepository.findByMessageId(
         MessageId(messageId)
